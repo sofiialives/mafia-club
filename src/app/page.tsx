69a04f60defdab06385ed2bd850ecc57d3cde556
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-03-26 22:05:34 +0100

    fix tables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Button from "@/components/Button";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import GameTable from "@/components/Table/GameTable";
 import GameTableBody from "@/components/Table/GameTableBody";
 import GameTableHeaders from "@/components/Table/GameTableHeaders";
 
 export default function Home() {
-  const header = ["Игрок", "Роли", "Фольі", "Бальі", "Д.Б"];
   const body = [
     { num: 1, text: "Texte", space: "s" },
     { num: 1, text: "Texte", space: "s" },
@@ -20,16 +18,27 @@ export default function Home() {
         <h1 className="text-yellow-300 text-4xl font-bold">Hello World</h1>
       </div>
       <GameTable
-        headers={header.map((h) => (
-          <GameTableHeaders action key={h}>
-            {h}
-          </GameTableHeaders>
-        ))}
+        headers={
+          <>
+            <tr>
+              <GameTableHeaders colSpan={14} action>
+                Игровые дейстия
+              </GameTableHeaders>
+            </tr>
+            <tr>
+              <GameTableHeaders colSpan={14} action active>
+                Игрок
+              </GameTableHeaders>
+            </tr>
+          </>
+        }
       >
         {body.map((b, index) => (
           <tr key={index}>
-            <GameTableBody>{b.num}</GameTableBody>
+            <GameTableBody action>{b.num}</GameTableBody>
             <GameTableBody>{b.text}</GameTableBody>
+            <GameTableBody>{b.space}</GameTableBody>
+            <GameTableBody>{b.space}</GameTableBody>
             <GameTableBody>{b.space}</GameTableBody>
             <GameTableBody>{b.space}</GameTableBody>
             <GameTableBody>{b.space}</GameTableBody>

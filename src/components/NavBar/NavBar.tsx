ab5af92cc32092f080fbb8fccb838a618dbd82a6
Виталий Беяр <vitaliy.beyar@gmail.com>
2024-04-10 20:55:42 -0230

    added not-fount page to all routs, added styles for active menu

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,27 +1,29 @@
-import Link from "next/link";
+"use client"
 import React from "react";
 import NavLinkItem from "./NavLinkItem.jsx"
+import { usePathname } from "next/navigation.js";
 
 type Props = {};
 const navMenu = [
   { id: "main", href: "/", title: "Главная" },
   { id: "about", href: "/#about", title: "О клубе" },
   { id: "contact", href: "/#contact", title: "Контакты" },
-  { id: "find", href: "/find-game", title: "Найти игру" },
+  { id: "find", href: "/find", title: "Найти игру" },
   { id: "game", href: "/game", title: "Играть" },
 ];
 
 const NavBar = (props: Props) => {
+  const pathname = usePathname();
   return (
     <nav>
       <ul className="flex gap-[60px] ">
         {navMenu.map((menu) => (
           <NavLinkItem
-            className=" text-[#FDD901] text-[28px] font-normal"
+            className="text-[#FDD901] text-[28px] font-normal relative"
             key={menu.id}
             href={menu.href}
             title={menu.title}
-            
+            current={pathname === menu.href}
           />
         ))}
       </ul>

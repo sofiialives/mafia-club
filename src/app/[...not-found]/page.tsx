c2d11c9edf9c93e69a817f0219920265c03a1541
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-04-25 15:32:14 +0200

    404 page

--- src/app/[...not-found]/page.tsx
+++ src/app/[...not-found]/page.tsx
@@ -1,14 +1,26 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import cn from "@/utils/cn";
 import Link from "next/link";
 import React from "react";
+import styles from "@/components/Table/input.module.css";
 
 interface NotFoundProps {}
 
 export default function NotFound({}: NotFoundProps) {
   return (
-    <div>
-      <h2>Не найдено</h2>
-      <p>Извините, страница которую вы ищите, не существует.</p>
-      <Link href="/">Вернуться на главную</Link>
-    </div>
+    <MaxWidthWrapper className="flex flex-col gap-18 justify-center items-center">
+      <video loop className="w-[800px]">
+        <source src="/404.mp4" type="video/mp4" />
+      </video>
+      <Link
+        href="/"
+        className={cn(
+          "border border-black rounded-full bg-[#FDD901] font-medium text-black text-base py-2 px-3",
+          styles.shadow
+        )}
+      >
+        Перейти на главную страницу
+      </Link>
+    </MaxWidthWrapper>
   );
 }
